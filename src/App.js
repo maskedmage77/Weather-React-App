ae46fe2df77e88a469cbeeb06251c71911fbd6f8
maskedmage77 <maskedmage77@gmail.com>
2021-07-16 21:01:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getCurrentWeather } from './weather-api/weather-api';
+import CurrentWeather from './CurrentWeather';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        let longitude , latitude;
+    
+        function locationExists(position) {
+            longitude = position.coords.longitude;
+            latitude = position.coords.latitude;
+    
+            getCurrentWeather(latitude + ',' + longitude)
+            .then((result) => {
+                console.log(result);
+                setLocalWeather(result);
+                setIsLoading(false);
+            }); 
+        }
+
+        // check user's current location
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(locationExists);
+        } else {
+            // insert a "cannot get location screen" here
+        }    
+    }, []);
+
+    // set current location's weather object
+    const [localWeather, setLocalWeather] = useState(null);
+    // used to toggle loading message while fetching data
+    const [isLoading, setIsLoading] = useState(true);
+
+    return (
+        <div className="App">
+            { isLoading && <div><h1>Loading...</h1></div> }
+            { localWeather && <CurrentWeather localWeather={localWeather}/> }
+        </div>
+    );
 }
 
 export default App;
